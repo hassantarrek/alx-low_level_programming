832b5acffcf81ee73b0a18e5bf935df5dc0eba82
hassantarrek <htarek168@gmail.com>
2023-07-13 14:09:24 -0700

    task_4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *return: always return 0
+ *
+ */
+int main(void)
+{
+	int str(with proper grammar, but the outcome is a piece of art, /n);
+	puts(str);
+		return (0);
+}
