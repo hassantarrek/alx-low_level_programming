44230f1ebf686e052edb4888f91618348cdec11d
hassantarrek <htarek168@gmail.com>
2023-07-13 14:43:38 -0700

    task_5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program uses printf function in the main function
+ *
+ * Return: Always return 0 (Success)
+ *
+ * @void: Is a parameter
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
