fc9d7eb5d4916a0b6c54d3b86f734abced71bcba
hassantarrek <htarek168@gmail.com>
2023-07-13 15:01:41 -0700

    task_6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Desription: This code fot alx task
+ *
+ * Return: Always return 0 (Success)
+ *
+ * @void: This is a parameter
+ *
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s) \nSize of an int: 4 byte(s) \nSize of a long int: 4 byte(s) \nSize of a long long int: 8 byte(s) \nSize of a float: 4 byte(s) \n");
+		return (0);
+}
