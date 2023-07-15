ae625a7f41c3eb63ad28318bf3e1ef0e71a860b2
hassantarrek <htarek168@gmail.com>
2023-07-14 18:15:49 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: for alx task
+ *
+ * Rerturn: always 0 (success)
+ */
+int main(void)
+{
+	int x = 97;
+
+	while (x <= 122)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
