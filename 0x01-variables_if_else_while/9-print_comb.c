392896a41cc412849fd63f0571593be397e96284
hassantarrek <htarek168@gmail.com>
2023-07-14 19:08:07 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: like every time lol
+ *
+ * Return: always 0 (lol again)
+ *
+ */
+int main(void)
+{
+	int abdo;
+
+	for (abdo = 48; abdo <= 57; abdo++)
+	{
+		putchar(abdo);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
