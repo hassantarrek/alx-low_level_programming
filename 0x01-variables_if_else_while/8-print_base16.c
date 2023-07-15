fcc341a205086de0c9805e1195a21f5a17d4798c
hassantarrek <htarek168@gmail.com>
2023-07-14 18:56:16 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: for alx task
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 15)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
