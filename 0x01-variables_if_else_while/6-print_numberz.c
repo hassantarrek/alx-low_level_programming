1869fb4038d0d029397cf3f8108f67379966decf
hassantarrek <htarek168@gmail.com>
2023-07-14 18:45:51 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: like every time lol
+ *
+ * Return: always 0 (lol again)
+ *
+ */
+int main(void)
+{
+	int x = 48;
+
+	while (x <= 57)
+	{
+		putchar(x);
+		x++;
+		}
+	putchar('\n');
+	return (0);
+}
