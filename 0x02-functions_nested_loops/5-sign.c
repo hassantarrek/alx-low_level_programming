f39b1772c78e5872915c629c2e8e9505f1742f1e
hassantarrek <htarek168@gmail.com>
2023-07-18 18:55:28 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ *
+ * @n: to be checked
+ *
+ * Return: 0 or 1 or -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+
+	else if (n < 0)
+	{
+		_putchar(5);
+		return (-1);
+	}
+
+	else
+		return (0);
+
+}
