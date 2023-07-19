eae9a3c1d3d3d6a45ae73ded3e7a231004991662
hassantarrek <htarek168@gmail.com>
2023-07-18 19:08:17 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ *
+ * @r: to be checked
+ *
+ * Return: the absolute value of n
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	return (-r);
+}
