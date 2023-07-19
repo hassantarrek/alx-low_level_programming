a81da50e0fbf7c157bf6bb79906ac4efb30c002c
hassantarrek <htarek168@gmail.com>
2023-07-18 17:28:44 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Starting point
+ *
+ * Description: 0-putchar.c
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+	return (0);
+}
