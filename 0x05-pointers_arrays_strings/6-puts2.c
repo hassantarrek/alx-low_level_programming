940548c19f76fe958f9a2f9d3c618db6618bad4f
hassantarrek <htarek168@gmail.com>
2023-07-27 14:11:22 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * starting with the first character, followed by a new line
+ * @str: string
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int = x;
+
+	for (x = 0; str[x] != 0; ++x)
+	{
+		if (x % 2 == 0)
+		{
+			_putchar(str[x]);
+		}
+	}
+	_putchar('\n');
+}
