6595afc6d9b1e5f8c7a14af705aaa1fa6c9b123e
hassantarrek <htarek168@gmail.com>
2023-08-04 17:56:54 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry pint
+ * @argc: int
+ * @argv: the other thing
+ * Return: 0
+ */
+int main(int argc, char const **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
