c601fefaaff4e299ecb8c559bb27d5e14ae23de4
hassantarrek <htarek168@gmail.com>
2023-08-10 16:30:53 -0700

    0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: int
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *x = malloc(b);
+
+	while (x == 0)
+	{
+		exit (98);
+	}
+	return (x);
+}
