e77c0c42563063609c7af248e175a8c436162897
hassantarrek <htarek168@gmail.com>
2023-08-14 17:59:50 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a lonely dog
+ * @d: init
+ * @name: lonely dog name
+ * @age: lonely dog age
+ * @owner: lonely dog owners name
+ * Return: VOID
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
