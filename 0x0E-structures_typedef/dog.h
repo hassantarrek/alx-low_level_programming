12e19641b619d4011f83d75c5b601b8f7279db7b
hassantarrek <htarek168@gmail.com>
2023-08-14 18:21:55 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,10 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/**
+ * dog_t - typedef
+ */
+typedef struct dog dog_t;
+
 #endif
