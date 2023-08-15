e3df666fddd43abb87a4462c33383aea1327e66e
hassantarrek <htarek168@gmail.com>
2023-08-14 18:42:57 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated for a struct dog
+ * @d: struc
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,5 +21,8 @@ void print_dog(struct dog *d);
  * dog_t - typedef
  */
 typedef struct dog dog_t;
-
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
