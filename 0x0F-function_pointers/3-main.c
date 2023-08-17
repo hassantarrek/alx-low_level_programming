4af49fcde5a21b417b9a86016ca2a9cfc1b27e71
hassantarrek <htarek168@gmail.com>
2023-08-17 13:08:54 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,15 +1,13 @@
-#include "function_pointers.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include "3-calc.h"
 /**
- * main - Prints the result of simple operations.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always 0.
+ * main - main function
+ * @argc: number of arguments
+ * @argv: array
+ * Return: 0
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int x, y;
 	char *op;
@@ -30,8 +28,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(99);
 	}
 
-	if ((*op == '/' && y == 0) ||
-	    (*op == '%' && y == 0))
+	if ((*op == '/' && y == 0) || (*op == '%' && y == 0))
 	{
 		printf("Error\n");
 		exit(100);
