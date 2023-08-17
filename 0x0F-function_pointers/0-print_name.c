e8793786ba6fa597a7768e946b6013e9ca97b27b
hassantarrek <htarek168@gmail.com>
2023-08-17 11:03:55 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,10 +3,10 @@
 /**
  * print_name - prints a name
  * @name: the name to be printed
- *
+ * @f: pointer
  * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-
+	f(name);
 }
