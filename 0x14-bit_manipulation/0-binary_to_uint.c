490342efe32e07dd5a6c242dd7d7d0ea15a96449
hassantarrek <htarek168@gmail.com>
2023-09-06 03:16:36 -0700

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * int binary_to_uint - converts a binary number
+ * @b: string
+ * Return:converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int x;
+	unsigned int decVal = 0;
+
+	if (!b)
+	{
+		return (0);
+	}
+	for (x = 0; b[x]; x++)
+	{
+		if (b[x] < '0' || b[x] > '1')
+		{
+			return (0);
+		}
+		decVal = 2 * decVal + (b[x] - '0');
+	}
+
+	return (decVal);
+}
